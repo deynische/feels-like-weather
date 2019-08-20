17aa5ebc762394f45e0a0b829bebe660f06db7be
Lynn Vu <dey.nische@gmail.com>
2019-08-19 22:58:21 -0700

    set up fetch api

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
+// CORS error workaround
+var url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + config.DARKSKY_KEY;
+
 window.onload = function() {
+    // nab html elements
+    var displayTitle = document.getElementById('weather');
     var displayInfo = document.getElementById('displayInfo');
+
+    // check for geolocation
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position){
-            var getData = position.coords.latitude + ' ' + position.coords.longitude;
-            displayInfo.append(getData);
+            // call Dark Sky API using geolocation
+            url += '/' + position.coords.latitude + ',' + position.coords.longitude;
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    displayInfo.innerHTML = data.currently.apparentTemperature + ' &deg;F';
+                    displayTitle.innerHTML = '';
+                })
+                .catch(error => console.error(error))
         });
     } else {
     
