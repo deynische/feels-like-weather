9dbaa8421465f185a8453552d296620d1b7733bb
Lynn Vu <dey.nische@gmail.com>
2019-08-20 21:05:00 -0700

    build and display Dark Sky API data

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A simple page showing the current weather using [Dark Sky API](https://darksky.n
 ## To Do
 - [x] Set up geolocation
 - [x] Set up API call
-- [ ] Format display data from API response
+- [x] Format display data from API response
 - [ ] Add temperature unit toggle
 - [ ] Format default display for no geolocation
 - [ ] Figure out geocoding for API call location
--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// CORS error workaround
+// CORS proxy server workaround for Dark Sky API
 var url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + config.DARKSKY_KEY;
 
 window.onload = function() {
@@ -14,7 +14,15 @@ window.onload = function() {
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    displayInfo.innerHTML = data.currently.apparentTemperature + ' &deg;F';
+                    console.log(data);
+                    var ambient = createElement('p',data.currently.apparentTemperature+'&deg;F','class','ambient');
+                    var forecast = createElement('p',data.currently.summary,'class','forecast');
+                    var wrapper = createElement('div','','class','wrapper');
+                    var hi = createElement('p','H '+data.daily.data[0].temperatureHigh+'&deg;');
+                    var lo = createElement('p','L '+data.daily.data[0].temperatureLow+'&deg;');
+                    var rh = createElement('p','RH '+(data.currently.humidity*100)+'%');
+                    wrapper.append(hi,lo,rh);
+                    displayInfo.append(ambient,forecast,wrapper);
                     displayTitle.innerHTML = '';
                 })
                 .catch(error => console.error(error))
@@ -23,3 +31,13 @@ window.onload = function() {
     
     };
 };
+
+// helper functions
+function createElement(el,content,attr,attrValue){
+    var newEl = document.createElement(el);
+    newEl.innerHTML = content;
+    if (attr){
+        newEl.setAttribute(attr,attrValue);
+    }
+    return newEl;
+}
