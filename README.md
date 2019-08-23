1a67d52ebf4a83b37c86645716163374e411e98f
Lynn Vu <dey.nische@gmail.com>
2019-08-22 20:14:08 -0700

    add text input and reverse geocoding; no search function yet

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Feels Like Weather
-A simple page showing the current ambient weather using [Dark Sky API](https://darksky.net/dev).
+A simple page showing the current ambient weather using [Dark Sky API](https://darksky.net/dev) and [OpenCage Geocoding API](https://opencagedata.com/).
 
 This project is based on the [freeCodeCamp local weather project](https://learn.freecodecamp.org/coding-interview-prep/take-home-projects/show-the-local-weather/).
 
 ## Notice
-As it is best practice to obfuscate API keys, this project will not work client-side without my API key. Current thoughts are to either implement CodePen's solution of prompting the user for an API key or to refactor the project to use the [freeCodeCamp Weather API](https://fcc-weather-api.glitch.me/).
+As it is best practice to obfuscate API keys, this project will not work client-side without my API keys. Current thoughts are to either implement CodePen's solution of prompting the user for an API key or to refactor the project to use the [freeCodeCamp Weather API](https://fcc-weather-api.glitch.me/).
 
 ## To Do
 - [x] Set up geolocation
 - [x] Set up API call
 - [x] Format display data from API response
 - [x] Add temperature unit toggle
-- [ ] If no geolocation, prompt for location
+- [x] Add text input for location search
 - [ ] Geocode location for weather API call
 - [ ] Format location error display
 - [ ] Prettify
+  - [ ] Obtain list of all possible weather returns from API
+  - [ ] Build list of background images per weather type
+  - [ ] Search forecast data for keywords to match a background image 
 - [ ] Refactor to work on GitHub pages
--- scripts.js
+++ scripts.js
@@ -1,21 +1,36 @@
 // CORS proxy server workaround for Dark Sky API
 var url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + config.DARKSKY_KEY;
-// unit flag
-var unit = ''
+var geocode = 'https://api.opencagedata.com/geocode/v1/json?q=';
 
 window.onload = function() {
     // html elements
+    var pageTitle = document.getElementsByTagName('h1');
     var displayTitle = document.getElementById('weather');
     var displayInfo = document.getElementById('displayInfo');
     var Fbtn = document.getElementById('degF');
     var Cbtn = document.getElementById('degC');
+    // unit flag
+    var unit = '';
     // event listeners
     Fbtn.addEventListener('click',convertTemp);
     Cbtn.addEventListener('click',convertTemp);
 
+    // location display
+    var displayLoc = createElement('span','','class','location');
+    pageTitle[0].prepend(displayLoc);
+
     // check for geolocation
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position){
+            // call OpenCage API for geocoding
+            fetch(geocode+position.coords.latitude+'+'+position.coords.longitude+'&key='+config.OPENCAGE_KEY)
+                .then(response => response.json())
+                .then(data => {
+                    // create if/then for: city/state, city/province, city/country
+                    console.log(data);
+                    displayLoc.innerHTML = data.results[0].components.city+', '+data.results[0].components.state_code;
+                })
+                .catch(error => console.error(error))
             // call Dark Sky API using geolocation
             url += '/'+position.coords.latitude+','+position.coords.longitude+'?units=auto';
             fetch(url)
@@ -30,7 +45,7 @@ window.onload = function() {
                     var wrapper = createElement('div','','class','wrapper');
                     var hi = createElement('span',Math.round(data.daily.data[0].temperatureHigh),'class','temp');
                     var lo = createElement('span',Math.round(data.daily.data[0].temperatureLow),'class','temp');
-                    var rh = createElement('p','RH '+(data.currently.humidity*100)+'%');
+                    var rh = createElement('p','RH '+Math.round(data.currently.humidity*100)+'%');
                     var hiLabel = createElement('p','H ');
                     var loLabel = createElement('p','L ');
                     hiLabel.append(hi);
